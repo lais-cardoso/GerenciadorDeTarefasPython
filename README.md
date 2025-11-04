20dba9e1cbec021022668507f554b672ea7beb90
Robert156x <rbpc156@gmail.com>
2025-11-04 19:28:57 -0300

    nnlembro

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ python main.py
 ```
 
 ## Caça Bugs
-- Depois de realizar a função de Adicionar tarefa ao tentar Listar Tarefas ele fala que nenhuma tarefa esta cadastrada
-- As palavras estao escritas como "tarefa" ao inves de "tarefas" nas funções 1, 3, 4, 5.
-- Ao tentar Concluir tarefa e Remover tarefa o codigo pede o Numero da tarefa mesmo que o numero não tenha sido pedido para ser definido em nenhum momento
-- As linhas 17,18 e 19,20 no tarefas.py estão duplicadas
+- Depois de realizar a função de Adicionar tarefa ao tentar Listar Tarefas ele fala que nenhuma tarefa esta cadastrada - Corretiva
+- As palavras estao escritas como "tarefa" ao inves de "tarefas" nas funções 1, 3, 4, 5. - Corretiva
+- Ao tentar Concluir tarefa e Remover tarefa o codigo pede o Numero da tarefa mesmo que o numero não tenha sido pedido para ser definido em nenhum momento - Perfectiva
+- As linhas 17,18 e 19,20 no tarefas.py estão duplicadas - Preventiva
--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def menu():
     print("2. Listar tarefas")
     print("3. Concluir tarefas")
     print("4. Remover tarefas")
-    print("5. Sair")
+    print("5. Buscar tarefas")
+    print("6. Total de tarefas")
+    print("7. Listar tarefas duplicadas")
+    print("8. Sair")
 
 def executar():
     while True:
@@ -18,6 +21,7 @@ def executar():
             descricao = input("Descrição: ")
             adicionar_tarefa(titulo, descricao)
         elif opcao == "2":
+            indice = int(input("Número da tarefa: ")) - 1
             listar_tarefas()
         elif opcao == "3":
             indice = int(input("Número da tarefa: ")) - 1
@@ -26,6 +30,15 @@ def executar():
             indice = int(input("Número da tarefa: ")) - 1
             remover_tarefa(indice)
         elif opcao == "5":
+            indice = int(input("Número da tarefa: ")) - 1
+            buscar_tarefa(indice)
+        elif opcao == "6":
+            indice = int(input("Número da tarefa: ")) - 1
+            total_tarefas(indice)
+        elif opcao == "7":
+            indice = int(input("Número da tarefa: ")) - 1
+            listar_tarefas_duplicada(indice)
+        elif opcao == "8":
             print("Saindo...")
             break
         else:
